06735dddc2bd9b7d14325aa54cc9cd2baffd17cb
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-27 16:03:26 -0300

    refactor: payment schema

--- src/schemas/payments-schema.ts
+++ src/schemas/payments-schema.ts
@@ -2,12 +2,14 @@ import { CreatePayment } from "@/protocols";
 import Joi from "joi";
 
 export const createPaymentSchema = Joi.object<CreatePayment>({
-  ticketId: Joi.number().min(1).required(),
+  ticketId: Joi.number().min(1).integer().required(),
   cardData: Joi.object({
     issuer: Joi.string().required(),
     number: Joi.number().required(),
     name: Joi.string().required(),
-    expirationDate: Joi.string().regex(/^((0?[1-9]|1[0-2])\/20[0-9]{2})$/).required(),
+    expirationDate: Joi.string()
+      .regex(/^((0?[1-9]|1[0-2])\/20[0-9]{2})$/)
+      .required(),
     cvv: Joi.number().required(),
   }).required(),
 });
